5a128a00e70a3cc9d8eb50a26dbe36f14e9c12cd
Tobias Allen <vd327@cam.ac.uk>
2023-10-19 12:45:30 +0100

    loop

--- ProcessCommandsImproved.py
+++ ProcessCommandsImproved.py
@@ -48,6 +48,11 @@ def processgcode(filestub, commands, kp=15.5, ki=0.13, kd=6.0, nozzletemp=210, b
     checklimits(extrusionfactor, 2)
     checklimits(retraction, 15)
 
+    offsets = []
+    for i in range(num_prints):
+        x = i % (round(num_prints ** 0.5))
+        offsets.append(x, y)
+
     output = ''
 
     for n in range(num_prints):
